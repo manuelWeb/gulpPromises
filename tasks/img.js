6fbaf069aa1d1b7b036e4666bba0d2f114e19f3a
macfly <manuel@webdesignord.fr>
2018-01-22 13:22:36 +0000

    correction task img : add return with promise or not otherwise it's bug

--- tasks/img.js
+++ tasks/img.js
@@ -1,22 +1,22 @@
 var gulp = require("gulp");
 // with Promise
-// module.exports = function () {
-//   gulp.task('img', function() {
-//     return Promise.all([
-//       new Promise( function(resolve, reject){
-//         gulp.src(['src/**/images/*.{png,jpg,gif}'])
-//         .pipe(gulp.dest('render'))
-//         .on('end', resolve)
-//       })
-//     ]).then( function () {
-//       console.log('task img ok task suivant : task slim...');
-//       gulp.start('slim');
-//     });
-//   })
-// }
 module.exports = function () {
   gulp.task('img', function() {
-    gulp.src(['src/**/images/*.{png,jpg,gif}'])
-    .pipe(gulp.dest('render'))
+    return Promise.all([
+      new Promise( function(resolve, reject){
+        gulp.src(['src/**/images/*.{png,jpg,gif}'])
+        .pipe(gulp.dest('render'))
+        .on('end', resolve)
+      })
+    ]).then( function () {
+      console.log('task img ok task suivant : task slim...');
+      gulp.start('slim');
+    });
   })
 }
+// module.exports = function () {
+//   gulp.task('img', function() {
+//     gulp.src(['src/**/images/*.{png,jpg,gif}'])
+//     .pipe(gulp.dest('render'))
+//   })
+// }
